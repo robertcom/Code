8be949109b8cbd3eeb3e728fe189b97ed9fd64b5
Robert Dyzman <robert@ittech.pl>
2019-12-08 15:59:00 +0100

    Removed relation between Company and Picture

---
+++ EFremote/Migrations/201912072314455_CompanyPictureRelation.cs
@@ -0,0 +1,24 @@
+﻿namespace EFremote.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CompanyPictureRelation : DbMigration
+    {
+        public override void Up()
+        {
+            DropIndex("dbo.Companies", new[] { "Logo_Id" });
+            RenameColumn(table: "dbo.Pictures", name: "Logo_Id", newName: "Company_Id");
+            CreateIndex("dbo.Pictures", "Company_Id");
+            DropColumn("dbo.Companies", "Logo_Id");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Companies", "Logo_Id", c => c.Int());
+            DropIndex("dbo.Pictures", new[] { "Company_Id" });
+            RenameColumn(table: "dbo.Pictures", name: "Company_Id", newName: "Logo_Id");
+            CreateIndex("dbo.Companies", "Logo_Id");
+        }
+    }
+}
---
+++ EFremote/Migrations/201912081454503_NoRelationCompanyPicture.cs
@@ -0,0 +1,22 @@
+﻿namespace EFremote.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NoRelationCompanyPicture : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Companies", "Logo_Id", "dbo.Pictures");
+            DropIndex("dbo.Companies", new[] { "Logo_Id" });
+            DropColumn("dbo.Companies", "Logo_Id");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Companies", "Logo_Id", c => c.Int());
+            CreateIndex("dbo.Companies", "Logo_Id");
+            AddForeignKey("dbo.Companies", "Logo_Id", "dbo.Pictures", "Id");
+        }
+    }
+}
